1bfb93e4119c43bb2bccfd2a82595bbfad57c161
ruinuola <419319655@qq.com>
2017-05-17 16:04:33 +0800

    学习到router

--- app/containers/List/index.jsx
+++ app/containers/List/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import React from 'react'
+import { hashHistory } from 'react-router'
 
-class Detail extends React.Component {
+class List extends React.Component {
 	render() {
 		return (
+			<div>
 				<p>List, url参数: /*{this.props.params.id}*/</p>
+				<ul>
+					{arr.map((item, index)=>{
+						return <li key={index} onClick={this.clickHandle.bind(this, item)}>js jump</li>
+					})}
+				</ul>
+			<div>
 			)
 	}
 }
